46a607153933d9887860939f2cb012a4b4da5b5f
SamuelRuwe <ruwesam@gmail.com>
2024-11-15 16:33:15 -0500

    initial commit

--- flake.nix
+++ flake.nix
@@ -2,18 +2,46 @@
   description = "Sam's NeoVim Flake";
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs";
+      url = "github:nixos/nixpkgs?ref=nixos-unstable";
     };
     neovim = {
-      url = "github:neovim/neovim/stable?dir=contrib";
+      url = "github:nix-community/neovim-nightly-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-  outputs = { self, nixpkgs, neovim }: {
-    packages.x86_64-linux.default = neovim.packages.x86_64-linux.neovim;
-    apps.x86_64-linux.default = {
-      type = "app";
-      program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
+    telescope-recent-files-src = {
+      url = "github:smartpde/telescope-recent-files";
+      flake = false;
     };
   };
+  outputs = { self, nixpkgs, neovim, telescope-recent-files-src }:
+    let
+      overlayFlakeInputs = prev: final: {
+        neovim = neovim.packages.x86_64-linux.neovim;
+
+        vimPlugins = final.vimPlugins // {
+          telescope-recent-files = import ./packages/vimPlugins/telescopeRecentFiles.nix {
+            src = telescope-recent-files-src;
+            pkgs = prev;
+          };
+        };
+      };
+
+      overlayMyNeovim = prev: final: {
+        myNeovim = import ./packages/samDoesNeovim.nix {
+          pkgs = final;
+        };
+      };
+
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ overlayFlakeInputs overlayMyNeovim ];
+      };
+
+    in {
+      packages.x86_64-linux.default = pkgs.myNeovim;
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${pkgs.myNeovim}/bin/nvim";
+      };
+    };
 }
